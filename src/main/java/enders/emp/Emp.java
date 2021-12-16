f84a1d80ae3b9e046842e1799f1d4deb074fa919
Ender Shiru <endertheeevee@outlook.com>
2021-12-16 12:57:37 -0800

    begin

---
+++ src/main/java/enders/emp/Emp.java
@@ -0,0 +1,10 @@
+package enders.emp;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Emp implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/enders/emp/client/EmpClient.java
@@ -0,0 +1,13 @@
+package enders.emp.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class EmpClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
